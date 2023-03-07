a923932a8cb734f5d7c0354df768ee7df15d35f5
Emmanuel Cajas <emmanuelcajasballesteros@gmail.com>
2023-03-07 22:58:34 +0100

    agregando cambios

--- N1ejercicio2/Main.java
+++ N1ejercicio2/Main.java
@@ -3,18 +3,15 @@ package tasca7.N1ejercicio2;
 
 public class Main {
 	
+	@SuppressWarnings("deprecation") 
 	public static void main(String[] args) {
-		
-	
-		@SuppressWarnings("deprecation")
+				
 		TrabajadorOnline trabajadorOnline =new TrabajadorOnline("Rodrigo","Cajas",16);
-		
-		@SuppressWarnings("deprecation")
+				
 		TrabajadorPresencial trabajadorPresencial = new TrabajadorPresencial("Lida", "Huaman", 25);
 		
 		trabajadorOnline.imprimirMensaje();
 		trabajadorPresencial.horasExtras();
-		
-		
+			
 	}
 }
